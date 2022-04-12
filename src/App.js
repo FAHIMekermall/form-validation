6493da7141b05a4e84677aad8c86c4e8595a2d40
Fahim <samiulbasirfahim360@hotmail.com>
2022-04-13 00:31:11 +0600

    name validation done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState({ value: "", error: "" })
+	const [number, setNumber] = useState({ value: "", error: "" })
+	const [email, setEmail] = useState({ value: "", error: "" })
+	const [password, setPassword] = useState({ value: "", error: "" })
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+	}
+
+	const handleNameInput = (event) => {
+		const namePart = event.target.value.split(" ")
+		console.log(namePart[namePart.length - 1])
+		if (namePart.length < 2) {
+			setName({ value: "", error: "name must be at least 2 characters" })
+		} else if (namePart[namePart.length - 1] == "") {
+			setName({ value: "", error: "name must be at least 2 characters" })
+		} else {
+			setName({ value: "", error: "" })
+		}
+	}
+	return (
+		<>
+			<form>
+				<input onChange={handleNameInput} type="text" name="" id="" />
+				<input type="number" name="" id="" />
+				<input type="email" name="" id="" />
+				<input type="password" name="" id="" />
+				<p>{name.error}</p>
+			</form>
+		</>
+	)
 }
 
-export default App;
+export default App
